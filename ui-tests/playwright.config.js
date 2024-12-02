56e6cc032dd2ddc8ccc9960ff131d69bc5e3afeb
quantstack <gjmooney@asu.edu>
2024-12-02 14:42:16 +0100

    Fix tests

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -5,10 +5,18 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
-  webServer: {
-    command: 'jlpm start',
-    url: 'http://localhost:8888/lab',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI
-  }
+  webServer: [
+    {
+      command: 'jlpm start:lab',
+      url: 'http://localhost:8888/lab',
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI
+    },
+    {
+      command: 'jlpm start:server',
+      url: 'http://localhost:8080',
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI
+    }
+  ]
 };
