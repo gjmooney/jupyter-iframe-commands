f6c1564e49fb581c5775fc08c3b70da3212730ee
Greg <gjmooney@asu.edu>
2025-02-18 13:38:55 +0100

    Change CSS

--- packages/css-extension/src/index.ts
+++ packages/css-extension/src/index.ts
@@ -17,9 +17,10 @@ const plugin: JupyterFrontEndPlugin<void> = {
     commands.addCommand('jupyter-import-css', {
       execute: args => {
         const styleTag = document.createElement('style');
-        styleTag.id = 'searchText';
 
+        styleTag.id = 'searchText';
         styleTag.innerText = args['parsedCSS'] as string;
+
         document.head.appendChild(styleTag);
       }
     });
